e388a51f4bf1d23a0d0f0878edbd00a36bdf75a6
Nwannnunu <ezinnevirtous@gmail.com>
2022-04-09 11:03:51 -0700

    UNIX opreating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * return Always 0 (success)
  */
 int main(void)
 {
-	write(l, "and that piece of art is useful\* - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
