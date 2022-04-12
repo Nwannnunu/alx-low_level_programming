60207b692ce71804549806a581de8a5c769c3b0d
Nwannnunu <ezinnevirtous@gmail.com>
2022-04-12 14:31:57 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main -Entry point of the program
+ * main -Entry point of the program
  *
  * Description:  this program prints _putchar and a new line
  * Return: Always 0 (Success)
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/** print_alphabet - prints the alphabet, in lowercase followed by a new
+ *
+ * Return: no return value
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while(c <= 'z');
+	putchar(c++);
+	putchar('\n');
+}
+
+
+
+
