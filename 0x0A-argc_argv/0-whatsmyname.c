dc55daae81932cd4d1742494caac7400dcecaa1f
Nwannnunu <ezinnevirtous@gmail.com>
2022-05-02 17:37:24 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * _attribute_((unused)): to warn about used variable @argc
+ * @argv: array tha contains the program command line argument
+ * Return: 0-success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
