10dc7cc99e5baf85a73b3f3a43558550284b1fdf
Nwannnunu <ezinnevirtous@gmail.com>
2022-06-01 13:59:44 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	for (i = 0; text_content[i] != '\0'; i++)
+	for (i = 1; text_content[i] != '\0'; i++)
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
