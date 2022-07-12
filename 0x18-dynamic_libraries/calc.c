27de93707c9cbd6699a35ee704cd4954862fb17f
Nwannnunu <ezinnevirtous@gmail.com>
2022-07-12 17:00:02 +0000

    task

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * add - function to add 2 integer
+ * a - first integer
+ * b - second integer
+ * return: Return a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/* sub - function to subtract two number*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/* mul - function to mulyiply two number*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/*div - Function to divide two number*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/* mod - function to derive the modulo of two number*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
